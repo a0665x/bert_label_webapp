f1250b042f51ed64181efefe49df88cd4641abfb
Allen_Huang <44718189+a0665x@users.noreply.github.com>
2023-01-03 18:28:27 +0800

    Add files via upload

---
+++ streamlit_bert_label.py
@@ -0,0 +1,181 @@
+import streamlit as st
+import json
+import os
+import pandas as pd
+from st_aggrid import AgGrid
+from utils.tool_funs import BlobDataTransaction , NoLoggingPolicy
+st.set_page_config(layout="wide")
+#========================================================================
+Login_Flag = None
+# st.set_page_config(layout="wide")
+st.sidebar.title("Azure Blob Storage Login")
+USER = st.sidebar.text_input("Label User:" , value="")
+storage_account_name = st.sidebar.text_input("Storage Account Name:", value="nlplabel")
+storage_account_key = st.sidebar.text_input("Storage Account Key:", type="password", value="8O4CcWidAmAv+sFdraQ6VTsANHgiutP2mlbWBex41vPyxKgkLVfE4W71GUVsPOqcKWo76AmhkG1a+AStfEUeFA==")
+container_name = st.sidebar.text_input("Container Name:", value="labelfile")
+connection_string = f"DefaultEndpointsProtocol=https;AccountName={storage_account_name};AccountKey={storage_account_key};EndpointSuffix=core.windows.net"
+@st.cache(allow_output_mutation=True)
+def login(USER,storage_account_name, storage_account_key, container_name ):
+    connection_string = f"DefaultEndpointsProtocol=https;AccountName={storage_account_name};AccountKey={storage_account_key};EndpointSuffix=core.windows.net"
+    DT = BlobDataTransaction('', '', connection_string, container_name)
+    return DT
+if st.sidebar.button('Login'):
+    DT = login(USER , storage_account_name, storage_account_key, container_name)
+    Login_Flag = True
+    if Login_Flag:
+        st.sidebar.write('Already Login')
+DT = login(USER,storage_account_name, storage_account_key, container_name)
+st.sidebar.write("""***""")
+#========================================================================
+SAVE_PATH = './Temp/label_data/'
+PROMPT_PATH = './Temp/prompt/'
+if os.path.exists(SAVE_PATH) is not True:
+    os.makedirs(SAVE_PATH)
+if os.path.exists(PROMPT_PATH) is not True:
+    os.makedirs(PROMPT_PATH)
+st.header('NLP label tools')
+label_style = st.sidebar.selectbox('What semantic annotation do you need to process?' , ('binary_level','level_self_defined','context_prompt'))
+label_json = {}
+st.sidebar.write("""***""")
+if label_style == 'binary_level':
+    assign_label   = st.sidebar.selectbox('🏷️ assign the context label',['0','1'])
+if label_style == 'level_self_defined':
+    label_num = st.sidebar.number_input('setting total number of labels:',min_value = 0 , max_value=99 , step=1 )
+    assign_label = st.sidebar.selectbox('choose your level number: ', range(2 if label_style == 'binary_level' else label_num if label_style == 'level_self_defined' else 2))
+if label_style == 'level_self_defined' or label_style == 'binary_level':
+    label_descript = st.sidebar.text_area('write the context description')
+    if os.path.exists(SAVE_PATH + label_style+"_label.json") == False:
+        with open(SAVE_PATH + f"{USER}_label.json", "w",encoding='utf-8') as outfile:
+            json.dump({'label_style': label_style}, outfile)
+    submitted = st.sidebar.button("📥 upload_label_description")
+    if submitted:
+        st.write(f"label:{assign_label}:{label_descript}")
+        with open(SAVE_PATH + f"{USER}_label.json", "r",encoding='utf-8') as file:
+            label_json = json.load(file)
+            label_json[assign_label] = label_descript
+        with open(SAVE_PATH + f"{USER}_label.json", "w",encoding='utf-8') as outfile:
+            json.dump(label_json, outfile)
+        st.sidebar.write('json path:'+SAVE_PATH +f"{USER}_label.json")
+
+    with open(SAVE_PATH +f"{USER}_label.json", "r", encoding='utf-8') as file:
+        label_json = json.load(file)
+    st.sidebar.json(label_json)
+    if st.sidebar.button('upload_json_to_Azure'):
+        DT.uploadJsonObjToBlobStorage(label_json , f'{USER}_label.json')
+
+    st.sidebar.write('<style>div.row-widget.stRadio > div{flex-direction:row;justify-content: center;} </style>',
+             unsafe_allow_html=True)
+    st.sidebar.write('<style>div.st-bf{flex-direction:column;} div.st-ag{font-weight:bold;padding-left:2px;}</style>',
+             unsafe_allow_html=True)
+    cancel = st.sidebar.radio('are you sure to cancel all label file?[Y/N]:', ('No' , 'Yes'))
+
+    if st.sidebar.button('🧹 reset_json_kernel') and cancel == 'Yes':
+        DT.removeDataFromBlob(SAVE_PATH + f"{USER}_label.json")
+        os.remove(SAVE_PATH + f"{USER}_label.json")
+
+    col1, col2 = st.columns([7,3],gap="large")
+    #=====================================================
+    with col1:
+        st.write("assign the context label")
+        st.write('<style>div.row-widget.stRadio > div{flex-direction:row;justify-content: center;} </style>',
+                unsafe_allow_html=True)
+        st.write('<style>div.st-bf{flex-direction:column;} div.st-ag{font-weight:bold;padding-left:2px;}</style>',
+                unsafe_allow_html=True)
+
+        slider_val = st.radio("🏷️ Label:",range(2 if label_style == 'binary_level' else label_num if  label_style == 'level_self_defined' else 0))
+        try:
+            with open(SAVE_PATH + f"{USER}_label.json", "r",encoding='utf-8') as file:
+                label_json = json.load(file)
+
+            show_discription = label_json[str(slider_val)]
+        except:
+            show_discription = 'Please write the description in the left sidebar'
+        with st.form("myform",clear_on_submit=True):
+            st.write('discription:',show_discription)
+            nlp_context = st.text_area('🗣️ write NLP label context on below✍🏻️:').replace('\n','')
+            # Every form must have a submit button.
+            # submitted = st.button("💌 Submit",on_click=clear_form)
+            submitted = st.form_submit_button("💌 Submit")
+            if submitted:
+                st.write("previous saved:\n", f'label:{slider_val} , context:{nlp_context}')
+                with open(SAVE_PATH + f"{USER}_NLP_dataset.csv", "a+",encoding='utf-8') as csv_file:
+                    csv_file.write(str(slider_val)+','+nlp_context+'\n')
+                st.write("csv_path:"+SAVE_PATH +f"{USER}_NLP_dataset.csv")
+        st.write("""***""")
+
+        if os.path.exists(SAVE_PATH + f"{USER}_NLP_dataset.csv"):
+            with open(SAVE_PATH + f"{USER}_NLP_dataset.csv", "r",encoding= 'utf-8') as csv_file:
+                lines = csv_file.readlines()[-3:]
+            df = pd.DataFrame(columns = ['label','context'])
+            for line in lines:
+                df1 = pd.DataFrame([[line[0] , line[2:]]],columns=['label', 'context'])
+                df = df.append(df1)
+            # st.dataframe(df)
+            st.write("show last 3 rows of label:")
+            AgGrid(df,theme='dark',  # Add theme color to the table
+                        enable_enterprise_modules=True,
+                        height=200,
+                        reload_data=False,
+                        wrap_text=True,
+                        resizeable=True)
+
+        if st.button('csv_to_Azure'):
+            DT.uploadToBlobStorage(SAVE_PATH + f"{USER}_NLP_dataset.csv" ,  f"{USER}_NLP_dataset.csv")
+        st.write("""***""")
+
+        cancel_csv = st.radio('are you sure to cancel all csv file?[Y/N]:', ('No', 'Yes'))
+        if st.button('🧹 reset_csv_kernel') and cancel_csv == 'Yes':
+            try:
+                DT.removeDataFromBlob(SAVE_PATH + f"{USER}_NLP_dataset.csv")
+                os.remove(SAVE_PATH + f"{USER}_NLP_dataset.csv")
+            except:
+                df = pd.DataFrame(columns=['label', 'context'])
+                st.write('The CSV file has already been cancelled.')
+    with col2:
+        uploaded_files = st.file_uploader("Choose a txt file", accept_multiple_files=True)
+        for uploaded_file in uploaded_files:
+            # bytes_data = uploaded_file.read()
+            # st.write("filename:", uploaded_file.name)
+            # st.write(bytes_data)
+            with open(PROMPT_PATH +uploaded_file.name,'r',encoding='utf-8') as path:
+                text = path.read()
+            st.write(f'prompt txt:{uploaded_file.name}')
+            st.write(text , encoding='utf-8')
+
+
+if label_style == 'context_prompt':
+    label_num = st.sidebar.number_input('setting total number of action:',min_value = 0 , max_value=99 , step=1)
+    label_descript = st.sidebar.text_area('write the context description')
+    st.header('😵 Allen_Huang is still designing the details of the labeling function, or you can buy him a coffee to console him 🔨🔨')
+
+    pass
+
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
+
+#streamlit run streamlit_bert_label.py --server.address 127.0.1.2 --server.port 80
+
+# pakage to exe
+# step1: upload projectfile to github
+# https://github.com/a0665x/Bert_Label_tool
+# setp2: go cloud streamlite and sign in by Github ID
+# https://streamlit.io/cloud
+# step3: choose py project for deploy and cook in oven process
+# step4: copy the web address (ex:https://a0665x-bert-label-tool-streamlit-bert-label-7mgm9w.streamlit.app/)
+# step5: install nativefier by using nmp install
+# npm install -g nativefier
+# step6: run cmd to packing
+# nativefier --name '<you .exe name>' '<your streamlit sharing website url>' --platform < 'windows' or 'mac' or 'linux' >
+# nativefier --name NLP_label_tool https://a0665x-bert-label-tool-streamlit-bert-label-uh7qs6.streamlit.app/ --platform windows --single-instance
+
+# or npm install -g appnativefy (for ubuntu)
+# appnativefy --name NLP_label_tool --url https://a0665x-bert-label-tool-streamlit-bert-label-7mgm9w.streamlit.app/ --services
+
+#nativefier --arch "x64" --platform "windows" --icon "C:\Users\User\PycharmProjects\bert_emot\project_demo\bert_label_tool\bert_emot.ico" --name "bert_emot_tool" "https://a0665x-bert-label-tool-streamlit-bert-label-uh7qs6.streamlit.app/" –-maximize –-always-on-top –-clear-cache –app-copyright "Allen_Huang" --app-version 1 –-disable-dev-tools –-tray "C:\Users\User\PycharmProjects\bert_emot\project_demo\bert_label_tool"
---
+++ utils/tool_funs.py
@@ -0,0 +1,156 @@
+from azure.storage.blob import BlobServiceClient , BlobClient
+import json
+from azure.core.pipeline.policies import HTTPPolicy
+from azure.core.pipeline import Pipeline
+from azure.core.pipeline.transport import RequestsTransport
+
+import pandas as pd
+
+# import logging
+# import azure.functions as func
+
+class NoLoggingPolicy(HTTPPolicy):
+    def send(self, request, **kwargs):
+        return self.next.send(request, **kwargs)
+class BlobDataTransaction:
+    def __init__(self,storage_account_key , storage_account_name , connection_string , container_name):
+        self.storage_account_key = storage_account_key
+        self.storage_account_name = storage_account_name
+        self.connection_string = connection_string
+        self.container_name = container_name
+
+    def uploadToBlobStorage(self, local_path, file_name):
+        blob_service_client = BlobServiceClient.from_connection_string(self.connection_string)
+        blob_client = blob_service_client.get_blob_client(container=self.container_name, blob=file_name)
+        with open(local_path, 'rb') as data:
+            blob_client.upload_blob(data, overwrite=True)
+        print(f'Uploaded data from {local_path} to {self.container_name}')
+    def uploadJsonObjToBlobStorage(self, dict_data_obj,file_name):
+        json_data = json.dumps(dict_data_obj)  # str_dict = json.dump(dict)
+        json_io = json_data.encode()
+
+        blob_service_client = BlobServiceClient.from_connection_string(self.connection_string)
+        blob_client = blob_service_client.get_blob_client(container=self.container_name, blob=file_name)
+        blob_client.upload_blob(json_io, overwrite=True)
+        print(f'Uploaded Object_data to {self.container_name}')
+
+    def uploadDfObjToBlob(self,dataframe, file_name):
+        # Convert the dataframe to a CSV string
+        csv_string = dataframe.to_csv(index=False)
+        # Create a BlobDataTransaction object
+        transaction = BlobDataTransaction(storage_account_key=self.storage_account_key,
+                                          storage_account_name=self.storage_account_name,
+                                          connection_string=self.connection_string,
+                                          container_name=self.container_name)
+        # Upload the CSV string to the blob
+        blob_service_client = BlobServiceClient.from_connection_string(transaction.connection_string)
+        blob_client = blob_service_client.get_blob_client(container=transaction.container_name, blob=file_name)
+        blob_client.upload_blob(csv_string, overwrite=True)
+        print(f'Uploaded data from dataframe to {transaction.container_name}')
+
+    def downloadFileFromBlobStorage(self,file_name, local_path):
+        # Create the BlobServiceClient object which will be used to create a container client
+        blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+
+        # Create a blob client using the container name and blob name
+        blob_client = blob_service_client.get_blob_client(container=self.container_name, blob=file_name)
+
+        # Download the blob to a file
+        with open(local_path, "wb") as download_file:
+            download_file.write(blob_client.download_blob().readall())
+        print(f"Downloaded {file_name} to {local_path}")
+
+    def downloadAsObjFromBlobStorage(self, file_name):
+        # Create the BlobServiceClient object which will be used to create a blob client
+        blob_service_client = BlobServiceClient.from_connection_string(self.connection_string)
+        # Create a blob client using the container name and blob name
+        blob_client = blob_service_client.get_blob_client(container=self.container_name, blob=file_name)
+        # Download the blob as a stream
+        blob_stream = blob_client.download_blob().readall()
+        # Check the file extension and convert the stream to the appropriate object
+        if file_name.endswith('.json'):
+            # Convert the stream to a JSON object
+            obj = json.loads(blob_stream)
+        elif file_name.endswith('.csv'):
+            # Convert the stream to a string
+            obj = blob_stream.decode()
+        else:
+            raise ValueError(f'Unsupported file extension: {file_name}')
+        return obj
+
+
+    def CheckBlobFlieExists(self,file_name_check):
+
+        transport = RequestsTransport()
+        pipeline = Pipeline(transport, policies=[NoLoggingPolicy()])
+
+        blob_client = BlobClient.from_connection_string(
+            conn_str=self.connection_string,
+            container_name=self.container_name,
+            blob_name=file_name_check,#blob_name = "hotel_article_1.txt"
+            pipeline=pipeline)
+        if blob_client.exists():
+            print(f'{file_name_check} already exist in Azure Blob container ({self.container_name})')
+            return True
+        else:
+            print(f'file not in Azure blob container ({self.container_name})')
+            return  False
+    def removeDataFromBlob(self, file_name):
+        # Create the BlobServiceClient object which will be used to create a blob client
+        blob_service_client = BlobServiceClient.from_connection_string(self.connection_string)
+        # Create a blob client using the container name and blob name
+        blob_client = blob_service_client.get_blob_client(container=self.container_name, blob=file_name)
+        # Delete the blob
+        try:
+            blob_client.delete_blob()
+            print(f'Successfully deleted {file_name} from {self.container_name}')
+        except:
+            print('file already has been removed')
+            raise
+
+# def main(req: func.HttpRequest) -> func.HttpResponse:
+#     logging.info('Python HTTP trigger function processed a request.')
+#
+#     # Read the request body
+#     body = req.get_body()
+#     text = body.decode('utf-8')
+#
+#     # Convert the text to a dictionary object
+#     text_dict = {"text": text}
+#
+#     # Create a BlobDataTransaction object
+#     transaction = BlobDataTransaction(storage_account_key=storage_account_key,
+#                                      storage_account_name=storage_account_name,
+#                                      connection_string=connection_string,
+#                                      container_name=container_name)
+#     # Upload the dictionary object to the blob
+#     transaction.uploadJsonObjToBlobStorage(text_dict, "NLP_dataset.csv")
+#
+#     return func.HttpResponse(f"Successfully added the text to the NLP_dataset.csv file: {text}")
+
+
+
+
+
+if __name__ == "__main__":
+    storage_account_key = "8O4CcWidAmAv+sFdraQ6VTsANHgiutP2mlbWBex41vPyxKgkLVfE4W71GUVsPOqcKWo76AmhkG1a+AStfEUeFA=="
+    storage_account_name = "nlplabel"
+    connection_string = "DefaultEndpointsProtocol=https;AccountName=nlplabel;AccountKey=8O4CcWidAmAv+sFdraQ6VTsANHgiutP2mlbWBex41vPyxKgkLVfE4W71GUVsPOqcKWo76AmhkG1a+AStfEUeFA==;EndpointSuffix=core.windows.net"
+    container_name = "labelfile"
+
+    DT = BlobDataTransaction(storage_account_key , storage_account_name , connection_string , container_name)
+    # ===file path upload===:
+    # DT.uploadToBlobStorage('./prompt_txt/hotel_article_1.txt', 'hotel_article_1.txt')
+    # ===data Object upload===:
+    data = dict({'Key':'Value'})
+    DT.uploadJsonObjToBlobStorage(data , 'allen_label.json')
+    # ===download blob path to localed===:
+    # DT.downloadFileFromBlobStorage('hotel_article_to_blob.json', './prompt_txt/hotel_article_to_local.json')
+    # DT.downloadFromBlobStorage('w_hotel.txt', './prompt_txt/w_hotel_blob.txt')
+    # ===download File(txt/json) to local as a return Object===:
+    obj = DT.downloadAsObjFromBlobStorage('allen_label.json')
+    print(obj)
+    # ===check filename exist on blob===:
+    DT.CheckBlobFlieExists('allen_label.json')
+    # # ===remove data from blob===:
+    DT.removeDataFromBlob('allen_label.json')
